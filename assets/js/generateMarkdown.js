033bcda71bde12475c2078e6ea259edf13bed42f
emily <emilycassel77@gmail.com>
2024-02-22 10:58:24 -0500

    functions finished, questions

--- assets/js/generateMarkdown.js
+++ assets/js/generateMarkdown.js
@@ -1,14 +1,41 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === "none"){
+    return "";
+  } return `![License](https://img.shields.io/badge/license-${license}-blue)`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === "MIT"){
+    return "[MIT](https://opensource.org/license/mit)"
+  }
+  if(license === "BSD"){
+    return "[BSD](https://opensource.org/license/bsd))"
+  }
+  if(license === "Apache"){
+    return "[Apache](https://opensource.org/license/apache))"
+  }
+  if(license === "GPL")
+    return "[GPL](https://opensource.org/license/gpl))"
+  if(license === "none")
+    return "[none]"
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license === "none"){
+    return ""; 
+  }
+  return `## **License**
+  License: ${license}
+
+  If you need more information, please visit: [https://www.choosealicense.com/](https://www.choosealicense.com/)`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -44,9 +71,6 @@ function generateMarkdown(data) {
 
   ##Questions 
   ${data.questions}
-
-
-
 `;
 }
 
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,94 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const generateMarkdown = require("./generateMarkdown.js")
+const fs = require("fs")
 
 
 // TODO: Create an array of questions for user input
     let question = [
-        ""
+        //name 
+        {
+            type: "input", 
+            messsage: "What is the title of your project?",//what asking 
+            name: "title"
+        },
+
+        //description 
+        {
+            type: "input", 
+            messsage: "?",//what asking 
+            name: "description"
+        },
+        {
+            type: "input", 
+            messsage: "?",//what asking 
+            name: "description"
+        },
+        {
+            type: "input", 
+            messsage: "What is the title of your project?",//what asking 
+            name: "description"
+        },
+
+
+        //installation 
+        {
+            type: "input", 
+            messsage: "What is the title of your project?",//what asking 
+            name: "installation"
+        },
+
+
+        //usage 
+        {
+            type: "input", 
+            messsage: "What is the title of your project?",//what asking 
+            name: "usage"
+        },
+
+        //contributing 
+        {
+            type: "input", 
+            messsage: "What is the title of your project?",//what asking 
+            name: "contributing"
+        },
+
+        //license 
+        {
+            type: "list", 
+            messsage: "What license do you want to use?",//what asking 
+            name: "license",
+            choices: [
+                "MIT",
+                "BSD", 
+                "Apache",
+                "GPL",
+                "none"
+            ]
+        }, 
+        //Tests
+        {
+            type: "input", 
+            messsage: "What is the title of your project?",//what asking 
+            name: "tests"
+        },
+        //questions 
+        {
+            type: "input", 
+            messsage: "What is the title of your project?",//what asking 
+            name: "questions"
+        },
+
+
+
+
+
+
+
+
+
+        
+        
     ];
 
 
@@ -12,7 +96,7 @@ function askQuestion(){
     let answer = [];
 
     for (var i = 0; i < question.length; i++){
-        let questionAnswer = prompt(questions[i]); 
+        let questionAnswer = prompt(question[i]); 
         answer.push(answer);
     }
     return answer;
@@ -20,16 +104,32 @@ function askQuestion(){
 
 let userIdea = askQuestion();
 
-for( var i =0; i <userIdea.length i++)
+for( var i =0; i <userIdea.length; i++)
 
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+    let markdownInformation = generateMarkdown(data);
 
+    fs.writefile(fileName, markdownInformation, (err) => {
+        if (err){
+            return console.log(err)
+        } 
+        console.log("Success")
+    })
 }
 
+
+writeToFile();
+
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(question)
+    .then(function (data){
+        let readMeFile = "README.md"
+        writeToFile(readMeFile, data);
+    })
+}
 
 // Function call to initialize app
 init();
